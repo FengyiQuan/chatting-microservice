3288538df3c9c2abc055bdc347a53640b783ef40
FengyiQuan <quan.f@husky.neu.edu>
2023-02-03 20:36:09 -0500

    no db service

--- flask/dao/db.py
+++ flask/dao/db.py
@@ -1,43 +1,43 @@
-from models.User import User
+# from models.User import User
 
-from pymongo import MongoClient, DESCENDING
-from werkzeug.security import generate_password_hash
-from bson import ObjectId
-import pymongo
+# from pymongo import MongoClient, DESCENDING
+# from werkzeug.security import generate_password_hash
+# from bson import ObjectId
+# import pymongo
 
-try:
-    client = MongoClient(host='test_mongodb',
-                         port=27017,
-                         username='root',
-                         password='pass',
-                         authSource="admin")
-except pymongo.errors.ConnectionFailure:
-    print("Failed to connect to server {}".format('mongodb://127.0.0.1:27017'))
+# try:
+#     client = MongoClient(host='test_mongodb',
+#                          port=27017,
+#                          username='root',
+#                          password='pass',
+#                          authSource="admin")
+# except pymongo.errors.ConnectionFailure:
+#     print("Failed to connect to server {}".format('mongodb://127.0.0.1:27017'))
 
-print(client)
+# print(client)
 
-chat_db = client.get_database("ChatDB")
-users_collection = chat_db.get_collection("users")
-# rooms_collection = chat_db.get_collection("rooms")
-# room_members_collection = chat_db.get_collection("room_members")
-messages_collection = chat_db.get_collection("messages")
-
-
-def save_user(username, email, password):
-    password_hash = generate_password_hash(password)
-    user = users_collection.insert_one({'username': username, 'email': email, 'password': password_hash})
-    # print(user)
-    return user
+# chat_db = client.get_database("ChatDB")
+# users_collection = chat_db.get_collection("users")
+# # rooms_collection = chat_db.get_collection("rooms")
+# # room_members_collection = chat_db.get_collection("room_members")
+# messages_collection = chat_db.get_collection("messages")
 
 
-def get_user_by_username(username):
-    user_data = users_collection.find_one({'username': username})
-    # print(user_data)
-    return User(str(user_data['_id']), user_data['username'], user_data['email'],
-                user_data['password']) if user_data else None
+# def save_user(username, email, password):
+#     password_hash = generate_password_hash(password)
+#     user = users_collection.insert_one({'username': username, 'email': email, 'password': password_hash})
+#     # print(user)
+#     return user
 
 
-def get_user_by_id(user_id):
-    user_data = users_collection.find_one({'_id': ObjectId(user_id)})
-    return User(str(user_data['_id']), user_data['username'], user_data['email'],
-                user_data['password']) if user_data else None
+# def get_user_by_username(username):
+#     user_data = users_collection.find_one({'username': username})
+#     # print(user_data)
+#     return User(str(user_data['_id']), user_data['username'], user_data['email'],
+#                 user_data['password']) if user_data else None
+
+
+# def get_user_by_id(user_id):
+#     user_data = users_collection.find_one({'_id': ObjectId(user_id)})
+#     return User(str(user_data['_id']), user_data['username'], user_data['email'],
+#                 user_data['password']) if user_data else None
--- flask/server.py
+++ flask/server.py
@@ -3,20 +3,20 @@ import logging
 from datetime import datetime
 
 from flask import Flask, flash, render_template, request, redirect, url_for
-from flask_login import logout_user, login_required, current_user, LoginManager, login_user
+# from flask_login import logout_user, login_required, current_user, LoginManager, login_user
 from flask_socketio import SocketIO, join_room, leave_room
 from pymongo.errors import DuplicateKeyError
 
-from dao.db import save_user, get_user_by_username, get_user_by_id
+# from dao.db import save_user, get_user_by_username, get_user_by_id
 
 # from is_safe_url import is_safe_url
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
+# login_manager = LoginManager()
+# login_manager.init_app(app)
+# login_manager.login_view = 'login'
 room = 'general'  # only one room for now
 
 
@@ -26,80 +26,80 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/user/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    if request.method == 'GET':
-        return render_template('auth/login.html')
-    else:
-        username = request.form['username']
-        password = request.form['password']
-        user = get_user_by_username(username)
-        if user and user.check_password(password):
-            login_user(user)
-            # print('login successful')
-            next = request.args.get('next')
-            # if True or not is_safe_url(next):
-            #     return abort(400)
-            return redirect(next or url_for('index'))
-        else:
-            flash('Invalid username or password', 'danger')
-            return redirect(url_for('login'))
-
-
-@app.route('/user/register', methods=['GET', 'POST'])
-def signup():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-
-    if request.method == 'GET':
-        return render_template('auth/signup.html')
-    else:
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        password2 = request.form['password2']
-        user = get_user_by_username(username)
-        if password != password2:
-            flash('Passwords do not match', 'danger')
-            return redirect(url_for('signup'))
-        if user:
-            flash('User already exists', 'danger')
-            return redirect(url_for('signup'))
-        else:
-            try:
-                save_user(username, email, password)
-                flash('Please sign in', 'success')
-                return redirect(url_for('login'))
-            except DuplicateKeyError:
-                flash('User already exists', 'danger')
-                return redirect(url_for('signup'))
-
-
-@app.route("/logout", methods=['POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+# @app.route('/user/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#
+#     if request.method == 'GET':
+#         return render_template('auth/login.html')
+#     else:
+#         username = request.form['username']
+#         password = request.form['password']
+#         user = get_user_by_username(username)
+#         if user and user.check_password(password):
+#             login_user(user)
+#             # print('login successful')
+#             next = request.args.get('next')
+#             # if True or not is_safe_url(next):
+#             #     return abort(400)
+#             return redirect(next or url_for('index'))
+#         else:
+#             flash('Invalid username or password', 'danger')
+#             return redirect(url_for('login'))
+#
+#
+# @app.route('/user/register', methods=['GET', 'POST'])
+# def signup():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#
+#     if request.method == 'GET':
+#         return render_template('auth/signup.html')
+#     else:
+#         username = request.form['username']
+#         email = request.form['email']
+#         password = request.form['password']
+#         password2 = request.form['password2']
+#         user = get_user_by_username(username)
+#         if password != password2:
+#             flash('Passwords do not match', 'danger')
+#             return redirect(url_for('signup'))
+#         if user:
+#             flash('User already exists', 'danger')
+#             return redirect(url_for('signup'))
+#         else:
+#             try:
+#                 save_user(username, email, password)
+#                 flash('Please sign in', 'success')
+#                 return redirect(url_for('login'))
+#             except DuplicateKeyError:
+#                 flash('User already exists', 'danger')
+#                 return redirect(url_for('signup'))
+#
+#
+# @app.route("/logout", methods=['POST'])
+# @login_required
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
 
 
 @app.route('/rooms/<room_id>/', methods=['GET'])
-@login_required
+# @login_required
 def view_room(room_id):
     # room = get_room(room_id)
     # if room:  # and is_room_member(room_id, current_user.username):
     # room_members = get_room_members(room_id)
     # messages = get_messages(room_id)
-    return render_template('chatroom/view_room.html', username=current_user.username, room=room)
+    return render_template('chatroom/view_room.html', room=room)
     # room_members=room_members)
     # messages=messages)
     # else:
     #     return "Room not found", 404
 
 
-# sockerio events
+# socketio events
 @socketio.on('send_message')
 def handle_send_message_event(data):
     app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
@@ -133,9 +133,9 @@ def handle_leave_room_event(data):
     socketio.emit('leave_room_announcement', data, room=data['room'])
 
 
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return 'Unauthorized', 401
+# @login_manager.unauthorized_handler
+# def unauthorized_handler():
+#     return 'Unauthorized', 401
 
 
 @app.errorhandler(500)
@@ -147,12 +147,12 @@ def server_error(e):
     """.format(e), 500
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    # print(user_id)
-    user = get_user_by_id(user_id)
-    # print(user)
-    return user
+# @login_manager.user_loader
+# def load_user(user_id):
+#     # print(user_id)
+#     user = get_user_by_id(user_id)
+#     # print(user)
+#     return user
 
 
 if __name__ == '__main__':
@@ -160,7 +160,6 @@ if __name__ == '__main__':
     # application on Google App Engine. See entrypoint in app.yaml.
     socketio.run(app)
     # app.run(host='127.0.0.1', port=8080, debug=True)
-
 
 # if __name__=='__main__':
 #     app.run(host="0.0.0.0", port=5000)
