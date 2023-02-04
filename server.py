d51d26b6a7a8951114eb3bf49dfb0089efc79948
FengyiQuan <quan.f@husky.neu.edu>
2023-02-03 22:18:19 -0500

    add

--- server.py
+++ server.py
@@ -155,9 +155,9 @@ def index():
 #     return user
 
 
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8080, log_level="info")
+# if __name__ == '__main__':
+    # import uvicorn
+    # uvicorn.run("server:app", host="0.0.0.0", port=8080, log_level="info")
 
-# if __name__=='__main__':
-#     app.run(host="0.0.0.0", port=5000)
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)
