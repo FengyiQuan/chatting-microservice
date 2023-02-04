6838d1024a65760dc40f06882e078d1924080485
FengyiQuan <quan.f@husky.neu.edu>
2023-02-03 23:56:16 -0500

    first

--- app/server.py
+++ app/server.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from flask import Flask, flash, render_template, request, redirect, url_for
 
 # from flask_login import logout_user, login_required, current_user, LoginManager, login_user
-from flask_socketio import SocketIO, join_room, leave_room
+# from flask_socketio import SocketIO, join_room, leave_room
 
 # from pymongo.errors import DuplicateKeyError
 
@@ -15,7 +15,7 @@ from flask_socketio import SocketIO, join_room, leave_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+# socketio = SocketIO(app)
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 # login_manager.login_view = 'login'
@@ -104,37 +104,37 @@ def view_room(room_id):
 
 
 # socketio events
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
-                                                                    data['room'],
-                                                                    data['message']))
-    print("{} has sent message to the room {}: {}".format(data['username'],
-                                                          data['room'],
-                                                          data['message']))
-    data['created_at'] = datetime.now().strftime("%d %b, %H:%M")
-    data['room'] = room
-    # save_message(data['room'], data['message'], data['username'])
-    socketio.emit('receive_message', data, room=data['room'])
-
-
-@socketio.on('join_room')
-def handle_join_room_event(data):
-    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
-
-    data['room'] = room
-    print("{} has joined the room {}".format(data['username'], data['room']))
-    join_room(data['room'])
-    socketio.emit('join_room_announcement', data, room=data['room'])
-
-
-@socketio.on('leave_room')
-def handle_leave_room_event(data):
-    app.logger.info("{} has left the room {}".format(data['username'], data['room']))
-    data['room'] = room
-    print("{} has left the room {}".format(data['username'], data['room']))
-    leave_room(data['room'])
-    socketio.emit('leave_room_announcement', data, room=data['room'])
+# @socketio.on('send_message')
+# def handle_send_message_event(data):
+#     app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
+#                                                                     data['room'],
+#                                                                     data['message']))
+#     print("{} has sent message to the room {}: {}".format(data['username'],
+#                                                           data['room'],
+#                                                           data['message']))
+#     data['created_at'] = datetime.now().strftime("%d %b, %H:%M")
+#     data['room'] = room
+#     # save_message(data['room'], data['message'], data['username'])
+#     socketio.emit('receive_message', data, room=data['room'])
+#
+#
+# @socketio.on('join_room')
+# def handle_join_room_event(data):
+#     app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+#
+#     data['room'] = room
+#     print("{} has joined the room {}".format(data['username'], data['room']))
+#     join_room(data['room'])
+#     socketio.emit('join_room_announcement', data, room=data['room'])
+#
+#
+# @socketio.on('leave_room')
+# def handle_leave_room_event(data):
+#     app.logger.info("{} has left the room {}".format(data['username'], data['room']))
+#     data['room'] = room
+#     print("{} has left the room {}".format(data['username'], data['room']))
+#     leave_room(data['room'])
+#     socketio.emit('leave_room_announcement', data, room=data['room'])
 
 
 # @login_manager.unauthorized_handler
@@ -160,4 +160,4 @@ def handle_leave_room_event(data):
 
 
 if __name__ == '__main__':
-    socketio.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
