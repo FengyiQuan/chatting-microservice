b2fd13a2a493d5220ea24d5ba84ebc7dc91262b2
FengyiQuan <quan.f@husky.neu.edu>
2023-02-03 22:56:45 -0500

    add

--- app/server.py
+++ app/server.py
@@ -158,9 +158,5 @@ def view_room(room_id):
 #     return user
 
 
-# if __name__ == '__main__':
-# import uvicorn
-# uvicorn.run("server:app", host="0.0.0.0", port=8080, log_level="info")
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
